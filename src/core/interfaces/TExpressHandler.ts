d1e6948b9b1b96cd2f13eb6b8d2b8f3193187e81
isaacfintec <isaac@fintecimal.com>
2023-07-11 17:49:33 -0600

    feat(app): server file was added

--- src/core/interfaces/TExpressHandler.ts
+++ src/core/interfaces/TExpressHandler.ts
@@ -1,6 +1,14 @@
 import { Response, NextFunction } from 'express';
 import { IRequest } from './Request';
 
+export interface AddressInfo {
+  address: string;
+  family: string;
+  port: number;
+}
+
+export type Address = AddressInfo | string | null;
+
 export type TExpressHandler = (
   req: IRequest,
   reply: Response,
